14d607d034455192886db87651b81ab8d2041ff0
itsthatMatthew <66915372+itsthatMatthew@users.noreply.github.com>
2022-10-11 09:53:16 +0200

    Update Fifo.java

--- lab05/thread/src/Fifo.java
+++ lab05/thread/src/Fifo.java
@@ -1,24 +1,19 @@
 import java.util.LinkedList;
 
 public class Fifo {
-    private LinkedList<String> container_;
-    public Fifo() {
-        container_ = new LinkedList<String>();
-    }
-    synchronized void put(String str) throws InterruptedException {
+    private LinkedList<String> container_ = new LinkedList<String>();
+    synchronized public void put(String str) throws InterruptedException {
         System.out.println("Fifo.put() " + Thread.currentThread().getId());
         while (container_.size() >= 10)
             this.wait();
         container_.add(str);
         notify();
     }
-    synchronized String get() throws InterruptedException {
+    synchronized public String get() throws InterruptedException {
         System.out.println("Fifo.get() " + Thread.currentThread().getId());
-        String ret;
         while (container_.isEmpty())
             this.wait();
-        ret = container_.getFirst();
-        container_.pop();
+        String ret = container_.pop();
         notify();
         return ret;
     }
