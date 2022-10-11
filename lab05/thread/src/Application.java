0776e10e1c7e89e7555ddd3f7e7ce3d41a909d0a
itsthatMatthew <66915372+itsthatMatthew@users.noreply.github.com>
2022-10-11 09:52:32 +0200

    Update Application.java

--- lab05/thread/src/Application.java
+++ lab05/thread/src/Application.java
@@ -1,15 +1,18 @@
+import java.util.Random;
+
 public class Application {
     public static void main(String[] args) throws InterruptedException {
+        Random rand = new Random();
         // közös Fifo tároló
         Fifo common = new Fifo();
         // Producerek
-        new Thread(new Producer(common, "1. producer", 1000)).start();
-        new Thread(new Producer(common, "2. producer", 1000)).start();
-        new Thread(new Producer(common, "3. producer", 1000)).start();
+        new Thread(new Producer(common, "1. producer", 1000 + (500 - rand.nextInt(1000)))).start();
+        new Thread(new Producer(common, "2. producer", 1000 + (500 - rand.nextInt(1000)))).start();
+        new Thread(new Producer(common, "3. producer", 1000 + (500 - rand.nextInt(1000)))).start();
         // Consumerek
-        new Thread(new Consumer(common, "1. consumer", 100)).start();
-        new Thread(new Consumer(common, "2. consumer", 100)).start();
-        new Thread(new Consumer(common, "3. consumer", 100)).start();
-        new Thread(new Consumer(common, "4. consumer", 100)).start();
+        new Thread(new Consumer(common, "1. consumer", 100 + (50 - rand.nextInt(100)))).start();
+        new Thread(new Consumer(common, "2. consumer", 100 + (50 - rand.nextInt(100)))).start();
+        new Thread(new Consumer(common, "3. consumer", 100 + (50 - rand.nextInt(100)))).start();
+        new Thread(new Consumer(common, "4. consumer", 100 + (50 - rand.nextInt(100)))).start();
     }
 }
